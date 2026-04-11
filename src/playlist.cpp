e8737324fd2201e599b7fc5323944e62d9c57454
Amadeus Nowe <amadeusnowe34@gmail.com>
2026-04-11 11:30:38 +0800

    modified playlist code, added functions

--- src/playlist.cpp
+++ src/playlist.cpp
@@ -2,3 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include "playlist.h"
+
+void addSong(Song *head){
+    if (head == NULL) {
+        printf("\nPlaylist is empty.\n");
+        return;
+    }
+}
+void playNext(){
+
+}
+void viewPlaylist(){
+
+}
+void showTotalDuration(){
+
+}
